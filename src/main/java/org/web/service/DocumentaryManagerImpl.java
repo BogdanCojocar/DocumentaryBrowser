bb4e9f1dcc3de91637c62a34e7bbf9c0d10ed43c
BogdanCojocar <bogdan.cojocar@gmail.com>
2014-12-23 17:20:51 +0200

    added try-catch block for invalid id deletion

--- src/main/java/org/web/service/DocumentaryManagerImpl.java
+++ src/main/java/org/web/service/DocumentaryManagerImpl.java
@@ -2,8 +2,10 @@ package org.web.service;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.orm.hibernate4.HibernateObjectRetrievalFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.web.data.Documentary;
@@ -14,15 +16,22 @@ import org.web.data.DocumentaryDAO;
 @ComponentScan({ "org.web.data" })
 public class DocumentaryManagerImpl implements DocumentaryManager {
 
+	private final static Logger log = Logger
+			.getLogger(DocumentaryManagerImpl.class.getName());
+
 	@Autowired
 	private DocumentaryDAO documentaryDAO;
 
 	public void saveOrUpdate(Documentary doc) {
 		documentaryDAO.saveOrUpdate(doc);
 	}
-	
+
 	public void delete(int id) {
-		documentaryDAO.delete(id);
+		try {
+			documentaryDAO.delete(id);
+		} catch (HibernateObjectRetrievalFailureException e) {
+			log.error("Invalid id: ", e);
+		}
 	}
 
 	public List<Documentary> getAllDocumentaries() {
